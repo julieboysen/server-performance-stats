2706675bc37bf62d93a6ade754ed33d07bafe50f
Julie <55697943+julieboysen@users.noreply.github.com>
2024-10-18 16:29:08 +0200

    added more stats

--- server-status.sh
+++ server-status.sh
@@ -1,3 +1,20 @@
+echo "OS version"
+hostnamectl | grep "Operating System"
+echo
+
+echo "Uptime"
+uptime | awk '{print substr($3, 1, length($3)-1)}'
+echo
+
+echo "Load average"
+uptime | awk '{print "Past minute: " substr($8, 1, length($8)-1)}'
+uptime | awk '{print "Past 5 minutes: " substr($9, 1, length($9)-1)}'
+uptime | awk '{print "Past 15 minutes: " $10}'
+echo
+
+echo "Logged in users"
+uptime | awk '{print $4}'
+echo
 
 echo "Total CPU usage"
 top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
